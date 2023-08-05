d7b9f0aa6ce40061900b8860571bc3d2d15826a5
EC2 Default User <ec2-user@ip-172-31-7-228.ap-northeast-1.compute.internal>
2023-08-05 13:07:20 +0000

    add favorite

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
-   belongs_to :user
-   has_one_attached :post_image
+ belongs_to :user
+ has_one_attached :post_image
+ has_many :favorites, dependent: :destroy
+ has_many :post_users, through: :favorite, source: :user
+
+ def favorited_by?(user)
+     favorites.exists?(user_id: user.id)
+ end
+
 end
