60547c69a36b723a98b8c1ff8214d310161a05e0
EC2 Default User <kanjyani.8.929@gmail.com>
2023-08-06 07:39:43 +0000

    fix

--- db/migrate/20230806053216_add_post_to_post_cafe_tag.rb
+++
@@ -1,6 +0,0 @@
-class AddPostToPostCafeTag < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :post_cafe_tags, :post, null: false, foreign_key: true
-    add_reference :cafe_tags, :cafe_tag, null: false,foreign_key: true
-  end
-end
--- db/migrate/20230806072415_create_post_cafe_tags.rb
+++ db/migrate/20230806072415_create_post_cafe_tags.rb
@@ -1,7 +1,8 @@
 class CreatePostCafeTags < ActiveRecord::Migration[6.1]
   def change
     create_table :post_cafe_tags do |t|
-
+      t.integer :post_id, null: false
+      t.integer :cafe_tag_id, null: false
       t.timestamps
     end
   end
