9accb7e6159a6b5c3d7cd8abdeeabc2604ae654c
EC2 Default User <kanjyani.8.929@gmail.com>
2023-08-08 05:53:24 +0000

    fix

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,29 +1,32 @@
 class Public::UsersController < ApplicationController
-  
+
   def show
     @user = current_user
     @user = User.find(params[:id])
     @following_users = @user.following_users
     @follower_users = @user.follower_users
   end
-  
+
   def edit
-    @user = @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
-  
+
   def update
     @user = current_user
       if @user.update(user_params)
-        flash[:notice] = "登録情報を変更しました"
-        redirect_to users_path
+        redirect_to user_path(@user)
       else
         render :edit
       end
   end
-  
+
   def unsubscribe
   end
-  
+
+  def index
+    @user = User.all
+  end
+
   def withdraw
     user = current_user
     user.update(is_deleted: true)
@@ -31,7 +34,7 @@ class Public::UsersController < ApplicationController
     flash[:notice] = "退会しました。"
     redirect_to new_user_registration_path
   end
-  
+
   def follows
     user = User.find(params[:id])
     @users = user.following_users
@@ -42,18 +45,18 @@ class Public::UsersController < ApplicationController
    user = User.find(params[:id])
    @users = user.follower_users
   end
-  
-  
+
+
   def favorites
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name,:email,:introduction,:profile_image )
   end
 end
-  
+
 
