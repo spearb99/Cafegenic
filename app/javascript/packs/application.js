7a315b2aa4f896d255e4da5bc3a6ce3c43f2b2bf
EC2 Default User <kanjyani.8.929@gmail.com>
2023-08-11 10:22:43 +0000

    add top

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,24 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"; 
 import "preview.js"
+import "script.js"
 
 Rails.start()
-Turbolinks.start()
+//Turbolinks.start()
 ActiveStorage.start()
+
+$(document).ready(function() {
+   // スライドショーの設定を記述します
+  $('.slider').slick({
+    autoplay: true, // 自動再生を有効にする
+    infinite: true, // スライドを無限ループさせる
+    speed: 500, // スライドの切り替わる速度を0.5秒に指定（ミリ秒単位）
+    slidesToShow: 3, // 一度に表示するスライドの数を3枚に指定
+    slidesToScroll: 1, // 一度にスクロールするスライドの数を1枚に指定
+    prevArrow: '<div class="slick-prev"></div>', // 前へボタンのHTMLコードを指定
+    nextArrow: '<div class="slick-next"></div>', // 次へボタンのHTMLコードを指定
+    centerMode: true, // センターモードを有効
+    variableWidth: true, // 変数幅を有効
+    dots: true, // ドット（ページネーション）を表示
+  });
+});
